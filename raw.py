72218b62a2977e08268ae4c2a8f8cc11a06f56d0
greg sidier <greg.sidier@gmail.com>
2011-10-20 23:29:15 +0100

    + raw.py

---
+++ raw.py
@@ -0,0 +1,38 @@
+from curs import cursor, All
+import dtype
+
+import mmap
+import numpy
+
+class RawCur(cursor):
+	
+	def __init__(self, data, type, name = '_'):
+		self.data = data
+		self.type = dtype.dtype(type)
+		self.name = name
+		self.columns = { self.name: self.type }
+		self.itemsize = numpy.dtype(self.type).itemsize
+		self.off = 0
+	
+	def fetch(self, n = None, cols = All):
+		bytes = self.data[self.off:self.off + n * self.itemsize]
+		a = numpy.fromstring(bytes, self.type)
+		nread = len(a)
+		self.off += len(bytes)
+		return {
+			self.name : a
+		}
+
+def write_raw(f, curs, col):
+	fetchsz = 4096
+	while 1:
+		cols = curs.fetch(fetchsz, [ col ])
+		a = cols[col]
+		if len(a) == 0:
+			break
+		a.tofile(f)
+
+def read_raw(f, type, name = '_'):
+	data = mmap.mmap(f.fileno(), 0, access = mmap.ACCESS_READ)
+	return RawCur(data, type, name)
+
