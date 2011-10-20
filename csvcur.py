c662ad931440a59998a1e9306b1b5fa5941f2141
greg sidier <greg.sidier@gmail.com>
2011-10-20 23:28:38 +0100

    Fix csvcur.py

--- csvcur.py
+++ csvcur.py
@@ -3,23 +3,29 @@ from curs import cursor, select, All
 import csv
 import numpy
 
-class csvcur(cursor):
+def _take(i, n):
+	for _ in xrange(n):
+		yield i.next()
+
+class CsvCur(cursor):
 	
-	def __init__(self, path):
-		self.path = path
-		f = file(path, 'r')
-		r = csv.reader(f, lineterminator = '\n')
-		header = r.next()
-		row = [ eval(x) for x in r.next() ]
-		self.columns = dict([
-			(name, numpy.array([val]).dtype)
-			for (name, val) in zip(header, row)
-		])
-		self.file = file(self.path, 'r')
-		self.reader = csv.reader(self.file, lineterminator = '\n')
-		self.reader.next() # skip header
+	def __init__(self, f, colnames, coltypes):
+		self.file = f
+		self.reader = csv.reader(f, lineterminator = '\n')
+		self.colnames = [ c for c in colnames ]
+		self.coltypes = [ numpy.dtype(t) for t in coltypes ]
+		self.columns = dict(zip(self.colnames, self.coltypes))
+		self.col_index = dict( (c,i) for (i,c) in enumerate(self.colnames) )
 	
 	def fetch(self, n = None, cols = All):
 		if n is None:
-			n = 10
-		
+			n = 100
+		if cols is All:
+			cols = self.colnames
+		I = [ self.col_index[c] for c in cols ]
+		print I
+		#rows = [ self.reader.next() for _ in xrange(n) ]
+		rows = list(_take(self.reader, n))
+		res = dict( (self.colnames[i], numpy.array([self.coltypes[i].type(r[i]) for r in rows])) for i in I )
+		return res
+
