ec2e7676d74004253617c1b55c58b14ca072b1d7
greg sidier <greg.sidier@gmail.com>
2011-10-25 01:58:02 +0100

    Detailed output.

--- test/mmap.py
+++ test/mmap.py
@@ -2,7 +2,8 @@ import util
 import mmap
 import numpy
 f = file('/usr/local/iso/openSUSE-11.3-DVD-x86_64.iso', 'r')
-m = mmap.mmap(f.fileno(), length=0, access=mmap.ACCESS_READ)
+m = mmap.mmap(f.fileno(), length=0, access=mmap.ACCESS_COPY)#READ)
 I = numpy.array(xrange(10000000), dtype=numpy.int) * 8
-res = util.mmap_random_access(m, I, I + 8)
-print len(res), "bytes"
+sz = 8
+res = util.mmap_random_access(m, I, I + sz)
+print "%d x %d = %d bytes" % (len(I), sz, len(res))
